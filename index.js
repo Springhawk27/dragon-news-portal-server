cfb0b0d980431485df2596a92bef57079078e170
Springhawk27 <sajjad0mahmud@gmail.com>
2022-10-20 19:42:52 +0600

    categories added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+var cors = require('cors')
+
+
+app.use(cors())
 
 const categories = require('./data/categories.json');
 
